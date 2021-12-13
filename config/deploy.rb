35029ec106dfbf53df79886a0d06f45e26e659d6
Eric-Guo <eric.guocz@gmail.com>
2021-12-13 14:33:01 +0800

    Go ruby 2.7.5

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ append :linked_dirs, *%w[log storage tmp/pids tmp/cache tmp/sockets public/uploa
 # set :ssh_options, verify_host_key: :secure
 
 set :rbenv_type, :user
-set :rbenv_ruby, "2.7.4"
+set :rbenv_ruby, "2.7.5"
 
 set :puma_init_active_record, true
 
