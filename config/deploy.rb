cb80274774d48e93948d48505a8c36d592b02962
Eric Guo <eric.guocz@gmail.com>
2024-03-11 17:32:15 +0800

    bump capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.18.0"
+lock "~> 3.18.1"
 
 set :application, "bluedoc"
 set :repo_url, "git@e.coding.net:sew/bluedoc.git"
